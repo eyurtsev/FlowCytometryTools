92121a8830a648d8dd52767bdf1f2879b7f26ad2
Eugene Yurtsev <eyurtsev@gmail.com>
2013-06-24 15:53:47 -0400

    updated __init__

--- __init__.py
+++ __init__.py
@@ -2,4 +2,8 @@ from _version import version as __version__
 from _doc import __doc__
 
 from core.containers import FCSample, FCPlate
+import core.graph as graph
 from core.graph import plotFCM, plot_histogram2d
+from core.util import build_FCplate_collection
+
+
