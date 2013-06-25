8fb691bf0be7778944fe71f0f725aa3a53dffcd8
Eugene Yurtsev <eyurtsev@gmail.com>
2013-06-25 10:30:44 -0400

    changed the list to a dictionary  (is that a good idea?)

--- core/util.py
+++ core/util.py
@@ -4,7 +4,7 @@ from containers import FCPlate
 
 def build_FCplate_collection(dirname=None, pattern='*.fcs', recursive=True):
     '''
-    Constructs a list of FCPlates by traversing recusively through the file hierarchy.
+    Constructs a dictionary of FCPlates by traversing recusively through the file hierarchy.
     All the fcs files under a given directory (on the same level) are grouped into a single FCPlate.
     The FCPlates are assigned the folders name as their ID.
     (NOTE: This means that the ID is not guaranteed to be unique.)
