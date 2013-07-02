61790eab6f5cb0eb015ca95210445c3b89af7beb
Eugene Yurtsev <eyurtsev@gmail.com>
2013-07-02 12:04:22 -0400

    added flowGUI to namespace

--- __init__.py
+++ __init__.py
@@ -5,5 +5,6 @@ from core.containers import FCSample, FCPlate
 import core.graph as graph
 from core.graph import plotFCM, plot_histogram2d
 from core.util import build_FCplate_collection
+from GUI import flowGUI
 
 
