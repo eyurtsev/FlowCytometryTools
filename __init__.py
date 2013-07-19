f5fb602307e9587145e77b6d9e3df6d71f81b841
Eugene Yurtsev <eyurtsev@gmail.com>
2013-07-19 17:32:08 -0400

    Imported fcs_parse into namespace

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
 from _version import version as __version__
 from _doc import __doc__
 
+from core.fcsreader import parse_fcs
 from core.containers import FCSample, FCPlate
 import core.graph as graph
 from core.graph import plotFCM, plot_histogram2d
 from core.util import build_FCplate_collection
+
 from GUI import flowGUI
 
 
