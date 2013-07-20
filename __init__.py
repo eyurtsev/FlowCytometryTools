19a14deea7f797d40f3b2f564687c51e67df10a9
Eugene Yurtsev <eyurtsev@gmail.com>
2013-07-20 15:48:06 -0400

    Moved fcsreader.py to io (from core)

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from _version import version as __version__
 from _doc import __doc__
 
-from core.fcsreader import parse_fcs
+from io.fcsreader import parse_fcs
 from core.containers import FCSample, FCPlate
 import core.graph as graph
 from core.graph import plotFCM, plot_histogram2d
