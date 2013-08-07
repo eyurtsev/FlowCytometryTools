0cf83a976a4dec8b3240f75d70524a5867c4f153
yonatanf <yonatanf@mit.edu>
2013-08-07 17:41:35 -0400

    added gates to init

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from _doc import __doc__
 
 from IO.fcsreader import parse_fcs
 from core.containers import FCSample, FCPlate, FCSampleCollection, FCOrderedCollection
+from core.gates import ThresholdGate, IntervalGate, QuadGate, PolyGate
 import core.graph as graph
 from core.graph import plotFCM, plot_histogram2d
 from core.util import build_FCplate_collection
