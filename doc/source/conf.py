41817234469e9707367732a9f0bb66359a0d8aef
Eugene Yurtsev <eyurtsev@gmail.com>
2013-10-12 16:03:35 -0400

    DOC: Updating conf to bootstrap (with proper theme link)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -193,7 +193,7 @@ html_theme_options = {
     # such as "amelia" or "cosmo".
     #
     # Note that this is served off CDN, so won't be available offline.
-    'bootswatch_theme': "http://netdna.bootstrapcdn.com/bootswatch/3.0.0/spacelab/bootstrap.min.css",
+    'bootswatch_theme': "", 
 
     # Choose Bootstrap version.
     # Values: "3" (default) or "2" (in quotes)
