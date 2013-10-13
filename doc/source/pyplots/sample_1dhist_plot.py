49b98ce383ee94fef39c05d4e8d69af4849512c4
Eugene Yurtsev <eyurtsev@gmail.com>
2013-10-13 15:50:19 -0400

    DOC: Adding gallery plots

---
+++ doc/source/pyplots/sample_1dhist_plot.py
@@ -0,0 +1,27 @@
+from FlowCytometryTools import FCMeasurement, ThresholdGate
+import os, FlowCytometryTools
+from pylab import *
+
+# Locate sample data included with this package
+datadir = os.path.join(FlowCytometryTools.__path__[0], 'tests', 'data', 'Plate01')
+datafile = os.path.join(datadir, 'RFP_Well_A3.fcs')
+
+# Load data
+tsample = FCMeasurement(ID='Test Plate', datafile=datafile).transform('hlog', ('Y2-A', 'B1-A', 'V2-A'))
+
+# Create gates
+y2_gate = ThresholdGate(1000.0, 'Y2-A', region='above')
+
+# Gate
+gated_sample = tsample.gate(y2_gate)
+
+# Plot
+ax1 = subplot(121);
+tsample.plot('Y2-A', gates=[y2_gate], apply_gates=False, bins=100);
+title('All Events');
+
+ax2 = subplot(122, sharey=ax1, sharex=ax1);
+gated_sample.plot('Y2-A', bins=100, color='y');
+title('Fluorescent Events');
+
+tight_layout()
