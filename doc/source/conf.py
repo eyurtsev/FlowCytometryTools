f36d368d7cef641226d163f78c0fb82ba599f055
Eugene Yurtsev <eyurtsev@gmail.com>
2013-10-13 14:37:15 -0400

    Class documentation taken from both class and __init__

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -29,6 +29,9 @@ sys.path.extend([
 
 ])
 
+# Takes documentation both from class doc and from __init__
+autoclass_content = 'both'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
