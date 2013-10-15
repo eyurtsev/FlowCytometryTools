88307031c20d5dbda06a28c59196fae480d64635
yonatanf <yonatanf@mit.edu>
2013-10-15 13:44:03 -0400

    FIX: tlog truncates to log10(th), rather than 0. Allows all positive th values.

--- core/transforms.py
+++ core/transforms.py
@@ -31,7 +31,7 @@ def tlog(x, th=1, r=_display_max, d=_l_mmax):
         values to be transformed.
     th : num
         values below th are transormed to 0.
-        Must be >=1.
+        Must be positive.
     r : num (default = 10**4)
         maximal transformed value.
     d : num (default = log10(2**18))
@@ -42,9 +42,9 @@ def tlog(x, th=1, r=_display_max, d=_l_mmax):
     -------
     Array of transformed values.
     '''
-    if th<1:
-        raise ValueError, 'Threshold value must be >=1. %s given.' %th
-    return where(x<=th, 0, log10(x) * 1.*r/d)
+    if th<=0:
+        raise ValueError, 'Threshold value must be positive. %s given.' %th
+    return where(x<=th, log10(th) * 1.*r/d, log10(x) * 1.*r/d)
 
 def tlog_inv(y, th=1, r=_display_max, d=_l_mmax):
     '''
@@ -57,7 +57,7 @@ def tlog_inv(y, th=1, r=_display_max, d=_l_mmax):
         values to be transformed.
     th : num
         Inverse values below th are transormed to th.
-        Must be >=1.
+        Must be > positive.
     r : num (default = 10**4)
         maximal transformed value.
     d : num (default = log10(2**18))
@@ -68,8 +68,8 @@ def tlog_inv(y, th=1, r=_display_max, d=_l_mmax):
     -------
     Array of transformed values.
     '''
-    if th<1:
-        raise ValueError, 'Threshold value must be >=1. %s given.' %th
+    if th<=0:
+        raise ValueError, 'Threshold value must be positive. %s given.' %th
     x = 10**(y * 1.*d/r)
     x[x<th] = th
     return x 
