ee4137d97c335d6367783f0a76f2355881bcda52
Eugene Yurtsev <eyurtsev@gmail.com>
2013-10-20 14:33:08 -0400

    Merging GUI from dev_wx

---
+++ GUI/gate_manager.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python
+"""
+Contains the gate manager which keeps track of the order of gates and allows
+user to choose which gates to interact with.
+"""
+from gate import PolygonGate, QuadGate, PolygonDrawer
+import util
+from util import call_wrapper
+from FlowCytometryTools import FCMeasurement
+from manager_states import STATE_GK
+from matplotlib.widgets import Cursor
+
+def debugging_print(msg):
+    return util.debugging_print(msg, True)
+
+class GateManager():
+    gate_num = 1
+    """ This will maintain a list of all the active gates. """
+    def __init__(self, ax, fig, gateList=None, parent=None):
+        if gateList is not None: GateManager.gateList = gateList
+        else: GateManager.gateList = []
+        self.current_channels = None
+        self.sample = None
+        self.fig =  fig
+        self.ax = ax
+        self.set_state(STATE_GK.WAITING)
+        self.parent = parent
+        self.connect()
+        self.cursorWidget = None
+        self._temp = None
+        self._plt_data = None
+
+    def connect(self):
+        #self.cidrelease = self.fig.canvas.mpl_connect('button_release_event', self.on_release)
+        self.cid_mouse_motion  = self.fig.canvas.mpl_connect('motion_notify_event', self.on_mouse_motion)
+        self.cid_mouse_press   = self.fig.canvas.mpl_connect('button_press_event', self.on_mouse_press)
+        self.cid_keyboard_press = self.fig.canvas.mpl_connect('key_press_event', self.on_keyboard_press)
+        #self.cidpick    = self.fig.canvas.mpl_connect('pick_event', self.on_mouse_pick)
+
+    def disconnect(self):
+        'disconnect all the stored connection ids'
+        ## TODO Define disconnect event properly
+        self.fig.canvas.mpl_disconnect(self.cid_mouse_press)
+        self.fig.canvas.mpl_disconnect(self.cid_mouse_motion)
+        self.fig.canvas.mpl_disconnect(self.cid_keyboard_press)
+
+    def on_mouse_motion(self, event):
+        """ Motion events. """
+        debugging_print(('Gate Keeper state: ', self.state))
+        if self.state == STATE_GK.START_DRAWING_QUAD_GATE:
+            if self.cursorWidget == None:
+                self.cursorWidget = Cursor(self.ax)
+
+    #@call_wrapper
+    def on_mouse_press(self, event):
+        """ Button press events. """
+        debugging_print(('Mouse Press. Gate Keeper state: ', self.state))
+        if self.state == STATE_GK.WAITING:
+            if self._temp is not None:
+                del self._temp
+                self._temp = None
+
+            ## Choose gate
+            if len(GateManager.gateList) > 0:
+                gates_to_inactivate = [thisGate for thisGate in GateManager.gateList if thisGate.state == 'Active']
+                activated_a_gate = False
+
+                for thisGate in GateManager.gateList:
+                    if thisGate.contains(event):
+                        if thisGate.state == 'Inactive':
+                            thisGate.activate()
+                        self.bring_gate_to_top_layer(thisGate)
+                        activated_a_gate = True
+
+                        if thisGate in gates_to_inactivate:
+                            gates_to_inactivate.remove(thisGate)
+                        break
+
+                for gate in gates_to_inactivate:
+                    if (activated_a_gate and gate is not thisGate) or not activated_a_gate:
+                        gate.inactivate()
+
+        elif self.state == STATE_GK.START_DRAWING_POLY_GATE:
+            debugging_print('Creating a polygon gate')
+            self.inactivate_all_gates()
+            gate = PolygonDrawer(channels=self.current_channels, gate_manager=self, new_gate_name=self.get_new_gate_name())
+            gate.on_press(event) # re raise last event
+            self._temp = gate
+            self.set_state(STATE_GK.KEEP_DRAWING)
+
+        elif self.state == STATE_GK.START_DRAWING_QUAD_GATE:
+            self.inactivate_all_gates()
+            name = self.get_new_gate_name()
+            quadGate = QuadGate(vert=(event.xdata, event.ydata),
+                    channels=self.current_channels,
+                    name=name, gate_manager=self, region='top left')
+            self.add_gate(quadGate)
+            self.cursorWidget = None
+            self.set_state(STATE_GK.WAITING)
+
+    def get_new_gate_name(self):
+        name = 'gate_{}'.format(GateManager.gate_num)
+        GateManager.gate_num += 1
+        return name
+
+    def on_keyboard_press(self, event):
+        if event.key == 'c':
+            self.set_state(STATE_GK.START_DRAWING_POLY_GATE)
+        elif event.key == 'w':
+            debugging_print(GateManager.gateList)
+
+    def set_axis(self, channels):
+        """
+        Sets the x and y axis
+        """
+        channels = tuple([ch.encode("UTF-8") for ch in channels]) # To get rid of u's
+
+        self.current_channels = channels
+        self.plot_data()
+        self.show_visible_gates()
+
+    def show_visible_gates(self):
+        #print 'Current channels :', self.current_channels
+        for thisGate in GateManager.gateList:
+            debugging_print(thisGate.channels)
+            debugging_print(self.current_channels)
+
+            #print 'Gate channels :', thisGate.channels
+            if thisGate.channels == self.current_channels:
+                #print 'setting gate visible'
+                thisGate.set_visible(True)
+            else:
+                thisGate.set_visible(False)
+        self.fig.canvas.draw()
+
+    #@call_wrapper
+    def inactivate_all_gates(self):
+        for gate in GateManager.gateList:
+            gate.inactivate()
+
+    #@call_wrapper
+    def add_gate(self, gate):
+        """ Adds the current gate to the gate list. """
+        GateManager.gateList.insert(0, gate)#append(gate)
+
+    def bring_gate_to_top_layer(self, gate):
+        GateManager.gateList.remove(gate)
+        GateManager.gateList.insert(0, gate)
+
+    def remove_gate(self, gate):
+        GateManager.gateList.remove(gate)
+        del gate
+
+    def get_active_gate(self):
+        for thisGate in GateManager.gateList:
+            if thisGate.active:
+                debugging_print('Active gate is: ')
+                debugging_print(thisGate)
+                return thisGate
+
+    #@call_wrapper
+    def set_state(self, state):
+        """ TODO Remove the handling of deleting gates here. """
+        if state == STATE_GK.DELETE_GATE:
+            activeGate = self.get_active_gate()
+            if activeGate:
+                activeGate.remove_gate()
+            state = STATE_GK.WAITING
+
+        self.ax.set_title(state)
+        self.fig.canvas.draw()
+        self.state = state
+
+    def plot_data(self):
+        """ Plots the loaded data """
+        sample = self.sample
+
+        ax = self.ax
+
+        if self._plt_data is not None:
+            if isinstance(self._plt_data, tuple):
+                # This is the case for histograms which return a tuple
+                patches = self._plt_data[2]
+                map(lambda x : x.remove(), patches)
+            else:
+                self._plt_data.remove()
+            del self._plt_data
+            self._plt_data = None
+
+        channels = self.current_channels
+
+        if channels[0] == channels[1]:
+            self._plt_data = sample.plot(channels[0], transform=('hlog', 'hlog'), ax=ax)
+            xlabel = self.current_channels[0]
+            ylabel = 'Counts'
+        else:
+            self._plt_data = sample.plot(channels, transform=('hlog', 'hlog'), ax=ax)
+            xlabel = self.current_channels[0]
+            ylabel = self.current_channels[1]
+
+
+
+        if hasattr(self._plt_data, 'get_datalim'):
+            bbox = self._plt_data.get_datalim(self.ax.transData)
+            p0 = bbox.get_points()[0]
+            p1 = bbox.get_points()[1]
+
+            self.ax.set_xlim(p0[0], p1[0])
+            self.ax.set_ylim(p0[1], p1[1])
+        else:
+            # Then it's a histogram?
+            xlims = self._plt_data[1]
+            xlims = (xlims[0], xlims[-1])
+            self.ax.set_xlim(xlims)
+            self.ax.set_ylim(0, max(self._plt_data[0]))
+
+        self.fig.canvas.draw()
+
+    def load_fcs(self, filepath=None, parent=None):
+        """
+        Loads an FCS file from the given filepath
+        """
+        if filepath is None:
+            from GoreUtilities import dialogs
+            filepath = dialogs.open_file_dialog('Select an FCS file to load', 'FCS files (*.fcs)|*.fcs', parent=parent)
+
+        # This can still be None if the user cancels in the dialog.
+        if filepath is not None:
+            self.sample = FCMeasurement('temp', datafile=filepath)
+            self._sample_loaded_event()
+
+    def load_measurement(self, measurement):
+        self.sample = measurement.copy()
+        self._sample_loaded_event()
+
+    def _sample_loaded_event(self):
+        if self.sample is not None:
+            if self.current_channels == None:
+                # Assigns first two channels by default if none have been specified yet.
+                self.current_channels = list(self.sample.channel_names[0:2])
+
+            self.set_axis(self.current_channels)
+            self.plot_data()
+
+    def get_generation_code(self):
+        """
+        Returns python code that generates all drawn gates.
+        """
+        code_list = [gate.get_generation_code() for gate in GateManager.gateList]
+        code_list.sort()
+        code_list = '\n'.join(code_list)
+        return code_list
---
+++ GUI/gui.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import wx
+from wireframe import GeneratedWireframe
+from manager_states import STATE_GK
+
+class GUIEmbedded(GeneratedWireframe):
+
+    def _update_axes(self):
+        # Quick fix. Should actually fire events
+        if self.fc_widget_ref.gate_manager.sample is not None:
+            options = list(self.fc_widget_ref.gate_manager.sample.channel_names)
+            options.sort() # May not be necessary
+            self.x_axis_list.Clear()
+            self.x_axis_list.InsertItems(options, 0)
+            self.y_axis_list.Clear()
+            self.y_axis_list.InsertItems(options, 0)
+
+    def load_fc_measurement(self, measurement):
+        self.fc_widget_ref.gate_manager.load_measurement(measurement)
+        self._update_axes()
+
+    def load_fcs(self, filepath=None):
+        self.fc_widget_ref.gate_manager.load_fcs(filepath=filepath, parent=self)
+        self._update_axes()
+
+    def btnLoadFCS(self, event):
+        self.load_fcs()
+
+    def btnQuitApp(self, event):
+        self.Close(1)
+
+    def btn_choose_x_channel(self, event):
+        if event.GetExtraLong(): # Quick hack
+            new_channel = event.GetString()
+            gate_manager = self.fc_widget_ref.gate_manager
+            current_channels = gate_manager.current_channels
+            gate_manager.set_axis((new_channel, current_channels[1]))
+
+    def btn_choose_y_channel(self, event):
+        if event.GetExtraLong(): # Quick hack
+            new_channel = event.GetString()
+            gate_manager = self.fc_widget_ref.gate_manager
+            current_channels = gate_manager.current_channels
+            gate_manager.set_axis((current_channels[0], new_channel))
+
+    def btn_create_poly_gate(self, event):
+        self.fc_widget_ref.gate_manager.set_state(STATE_GK.START_DRAWING_POLY_GATE)
+
+    def btn_create_quad_gate(self, event):
+        self.fc_widget_ref.gate_manager.set_state(STATE_GK.START_DRAWING_QUAD_GATE)
+
+    def btn_gen_code(self, event):
+        generated_code = self.fc_widget_ref.gate_manager.get_generation_code()
+        self.tb_gen_code.SetValue(generated_code)
+
+class FC_GUI(wx.App):
+    def OnInit(self):
+        wx.InitAllImageHandlers()
+        self.sample_viewer = GUIEmbedded(None, -1, "")
+        self.SetTopWindow(self.sample_viewer)
+        self.sample_viewer.Show()
+        return 1
+
+    def load_fcs(self, filepath):
+        self.sample_viewer.load_fcs(filepath)
+
+    def load_fc_measurement(self, measurement):
+        self.sample_viewer.load_fc_measurement(measurement)
+
+def parse_input():
+    """
+    Examples of use:
+        Opens up the specified FCS file with channels showing B1-A and Y2-A
+        python flowGUI.py ../tests/data/Plate01/CFP_Well_A4.fcs -c B1-A Y2-A
+    """
+    import argparse
+    epilog = parse_input.__doc__
+
+    parser = argparse.ArgumentParser(epilog=epilog, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument(metavar="FILE", dest="filename", help='fcs file to open', nargs='?', default=None)
+
+    parser.add_argument("-c", "--channel-names", dest="channel_names", nargs='+', help="channel names to plot on the x and y axis")
+
+    return parser.parse_args()
+
+
+def launch_from_fc_measurement(measurement):
+    """
+    Launches the GUI from an FCmeasurement.
+    """
+    app = FC_GUI()
+    app.load_fc_measurement(measurement)
+    app.MainLoop()
+
+
+
+if __name__ == "__main__":
+    import glob
+    import os
+    import FlowCytometryTools
+    datadir = os.path.join(FlowCytometryTools.__path__[0], 'tests', 'data', 'Plate01', '*.fcs')
+    files = glob.glob(datadir)[0]
+
+    #files = glob.glob('./*.fcs')[0]
+    #files = glob.glob('datadir/')[0]
+    print files
+    app = FC_GUI()
+    app.load_fcs(files)
+    app.MainLoop()
