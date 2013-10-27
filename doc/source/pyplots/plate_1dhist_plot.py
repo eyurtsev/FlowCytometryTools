0861a3b44d4b76dcfbe1da9fac39e187ef3403ae
Eugene Yurtsev <eyurtsev@mit.edu>
2013-10-27 15:27:19 -0400

    Updated plate example to work with API changes

--- doc/source/pyplots/plate_1dhist_plot.py
+++ doc/source/pyplots/plate_1dhist_plot.py
@@ -7,7 +7,7 @@ datadir = os.path.join(FlowCytometryTools.__path__[0], 'tests', 'data', 'Plate01
 datafile = os.path.join(datadir, 'RFP_Well_A3.fcs')
 
 # Load plate
-plate = FCPlate.from_dir(ID='Demo Plate', path=datadir).transform('hlog', channels=('Y2-A', 'B1-A'))
+plate = FCPlate.from_dir(ID='Demo Plate', path=datadir, parser='name').transform('hlog', channels=('Y2-A', 'B1-A'))
 
 # Drop empty cols / rows
 plate = plate.dropna()
@@ -15,3 +15,4 @@ plate = plate.dropna()
 # Plot
 plate.plot('Y2-A', bins=100);
 
+# show()
