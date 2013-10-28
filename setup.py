25b3ed20c151592e6bb43e50fc8dd0f29f46656b
Eugene Yurtsev <eyurtsev@mit.edu>
2013-10-27 21:10:34 -0400

    PyPI: updated manifest, setup and updated .gitignore as well.

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
-from FlowCytometryTools._version import version
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup, find_packages
+## get version info
+import re
+VERSIONFILE="FlowCytometryTools/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^version = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    version = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
 setup(
     name = 'FlowCytometryTools',
-    packages = ['FlowCytometryTools', 'FlowCytometryTools.tests', ],
+    #packages = ['FlowCytometryTools', 'FlowCytometryTools.tests', 'FlowCytometryTools.IO'],
+    packages=find_packages(),
     version = version,
     description = 'A python package for performing flow cytometry analysis',
     author = 'Jonathan Friedman, Eugene Yurtsev',
@@ -12,15 +23,25 @@ setup(
     download_url = 'https://bitbucket.org/gorelab/flowcytometrytools/get/v{0}.zip'.format(version),
     keywords = ['flow cytometry', 'data analysis', 'cytometry', 'single cell'],
     license='MIT',
-    install_requires=[
-          "pandas >= 0.8.0",
-      ],
+    dependency_links = ['https://bitbucket.org/gorelab/goreutilities/get/v{0}.zip#egg=GoreUtilities-0.3.0'.format(version)],
+    #dependency_links = ['https://bitbucket.org/gorelab/goreutilities/get/master.zip#egg=GoreUtilities-0.3.0'.format(version)],
 
+    install_requires=[
+          "setuptools",
+          "pandas >= 0.8.0",
+          "GoreUtilities == 0.3.0",
+      ],
     classifiers = [
         'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 2',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Topic :: Scientific/Engineering :: Medical Science Apps.',
-    ]
+    ],
+
+    include_package_data = True,
+    package_data = {
+        '': ['*.fcs'],
+    },
+
 
 )
