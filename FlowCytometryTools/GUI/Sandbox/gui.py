f39d6d2332d712799789eb82d8d9d0d751d5316f
Eugene Yurtsev <eyurtsev@gmail.com>
2013-11-26 16:37:37 -0500

    Updated GUI to include a Manager class

--- FlowCytometryTools/GUI/Sandbox/gui.py
+++ FlowCytometryTools/GUI/Sandbox/gui.py
@@ -1,32 +1,27 @@
+import matplotlib
+matplotlib.use('wxagg')
 from matplotlib.widgets import  RectangleSelector, Cursor, AxesWidget
 from pylab import *
 import pylab as pl
 from numpy import random
 import numpy
 
+
 class MOUSE:
     LEFT_CLICK = 1
     RIGHT_CLICK = 3
 
-
-class STYLE:
-    InactivePolyGate = {'color' : 'black', 'linestyle' : 'solid', 'fill' : False}
-    ActivePolyGate = {'color' : 'red', 'fill' : False}
-
-    #SuggestToActivateGate = {'color' : 'red', 'fill' : 'gray', 'alpha' : 0.4}
-    TemporaryPolyGateBorderLine = {'color' : 'black'}
-    PolyGateBorderLine = {'color' : 'black',  'linestyle' : 'None', 'marker':'s', 'mfc':'r', 'alpha':0.6}
-
-    InactiveQuadGate = {'color' : 'black', 'linewidth' : 1}
-    ActiveQuadGate   = {'color' : 'red', 'linewidth' : 2}
-    InactiveQuadGateCenter = {'color' : 'black', 'marker' : 's', 'markersize' : 8}
-    ActiveQuadGateCenter   = {'color' : 'red', 'marker' : 's', 'markersize' : 8}
-
 class Vertex(AxesWidget):
-    def __init__(self, coordinates, ax=None, update_notify_callback=None):
+    """
+    TODO Finish trackx, tracky to include mixed coordinate system.
+    """
+    def __init__(self, coordinates, ax=None, update_notify_callback=None,
+                            trackx=True, tracky=True):
         AxesWidget.__init__(self, ax)
         self.update_notify_callback = update_notify_callback
         self.selected = False
+        self.trackx = trackx
+        self.tracky = tracky
         self.coordinates = coordinates
         self.create_artist()
         self.connect_event('pick_event', lambda event : self.pick(event))
@@ -35,7 +30,9 @@ class Vertex(AxesWidget):
         self.connect_event('button_release_event', lambda event : self.mouse_button_release(event))
 
     def create_artist(self):
-        self.artist = pl.Line2D([self.coordinates[0]], [self.coordinates[1]], picker=10, marker='s', color='r', ms=10)
+        verts = self.coordinates
+        self.artist = pl.Line2D([verts[0]], [verts[1]], picker=10,
+                                        marker='s', color='r', ms=10)
         self.ax.add_artist(self.artist)
 
     def ignore(self, event):
@@ -65,9 +62,12 @@ class Vertex(AxesWidget):
             self._update()
 
     def update_position(self, xdata, ydata):
-        self.coordinates = xdata, ydata
-        self.artist.set_xdata([xdata])
-        self.artist.set_ydata([ydata])
+        if self.trackx:
+            self.coordinates = xdata, self.coordinates[1]
+            self.artist.set_xdata([xdata])
+        if self.tracky:
+            self.coordinates = self.coordinates[0], ydata
+            self.artist.set_ydata([ydata])
 
     def _update(self):
         if self.update_notify_callback is not None:
@@ -88,29 +88,17 @@ class PolyGate(AxesWidget):
         xy = [vertex.coordinates for vertex in self.vertex_list]
         self.poly.set_xy(xy)
 
-class DoubleVertex(AxesWidget):
-    def __init__(self, verts, ax=None):
-        AxesWidget.__init__(self, ax)
-        update_notify_callback = lambda vertex : self.update_position(vertex)
-        self.vertex1 = Vertex(verts, ax, update_notify_callback=update_notify_callback)
-        verts2 = list(verts)
-        verts2[0] = verts[0] + 2.4
-        self.vertex2 = Vertex(verts2, ax, update_notify_callback=update_notify_callback)
-
-    def update_position(self, vertex):
-        xdata, ydata = vertex.coordinates
-
-        if vertex == self.vertex1:
-            self.vertex2.update_position(xdata + 2.4, ydata)
-        else:
-            self.vertex1.update_position(xdata - 2.4, ydata)
-
 class ThresholdGate(AxesWidget):
     def __init__(self, verts, orientation='both', ax=None):
         self.orientation = orientation
         AxesWidget.__init__(self, ax)
         update_notify_callback = lambda vertex : self.update_position(vertex)
-        self.vertex = Vertex(verts, ax, update_notify_callback=update_notify_callback)
+
+        trackx = orientation in ['both', 'vertical']
+        tracky = orientation in ['both', 'horizontal']
+
+        self.vertex = Vertex(verts, ax, update_notify_callback=update_notify_callback,
+                    trackx=trackx, tracky=tracky)
         self.create_artist()
 
     def create_artist(self):
@@ -118,10 +106,10 @@ class ThresholdGate(AxesWidget):
         self.artist_list = []
 
         if self.orientation in ('both', 'horizontal'):
-            self.hline = self.ax.axhline(y=vert[1], **STYLE.ActiveQuadGate)
+            self.hline = self.ax.axhline(y=vert[1])
             self.artist_list.append(self.hline)
         if self.orientation in ('both', 'vertical'):
-            self.vline = self.ax.axvline(x=vert[0], **STYLE.ActiveQuadGate)
+            self.vline = self.ax.axvline(x=vert[0])
             self.artist_list.append(self.vline)
 
     def update_position(self, vertex):
@@ -201,63 +189,131 @@ class PolyDrawer(AxesWidget):
         self.line.remove()
 
 class Manager():
-    gates = []
-
-def create_vertex(event):
-    #rect = DraggableRectangle.from_vert(plt.gca(), (event.xdata, event.ydata, 0.3, 0.3))
-    rect = Vertex((event.xdata, event.ydata), plt.gca())
-    Manager.gates.append(rect)
-    Manager.cs.clear(event)
-    Manager.cs.disconnect_events()
-    del Manager.cs
-    gcf().canvas.draw()
-
-def create_threshold_gate(event):
-    #rect = DraggableRectangle.from_vert(plt.gca(), (event.xdata, event.ydata, 0.3, 0.3))
-    #rect = DoubleVertex((event.xdata, event.ydata), 'vertical', plt.gca())
-    rect = DoubleVertex((event.xdata, event.ydata), plt.gca())
-    Manager.gates.append(rect)
-    Manager.th.clear(event)
-    Manager.th.disconnect_events()
-    del Manager.th
-    gcf().canvas.draw()
-
-def gate_drawer(event):
     """
-    Launches different drawing tools
+    Manages gate creation widgets.
     """
-    ax = plt.gca()
-    if event.key in ['R', 'r']:
-        print 'Launching rectangle selector'
-        onselect.RS = RectangleSelector(ax, onselect, drawtype='box')
-    elif event.key in ['T', 't']:
-        print 'Launching circle creator'
-        if not hasattr(Manager, 'th'):
-            Manager.th = Cursor(ax)
-            Manager.th.connect_event('button_press_event', create_threshold_gate)
-    elif event.key in ['C', 'c']:
-        print 'Launching circle creator'
-        if not hasattr(Manager, 'cs'):
-            Manager.cs = Cursor(ax)
-            Manager.cs.connect_event('button_press_event', create_vertex)
-    elif event.key.lower() in ['p']:
-        print 'Launching circle creator'
-        Manager.pd = PolyDrawer(ax, oncreated=create_polygon, lineprops = dict(color='k', marker='o'))
-#Manager.rect = []
+    def __init__(self):
+        self.gates = []
+        fig = figure()
 
-def create_polygon(poly_drawer_instance):
-    verts = poly_drawer_instance.verts
-    ax = poly_drawer_instance.ax
-    pg = PolyGate(verts, ax)
-    Manager.pg = pg
-    del poly_drawer_instance
-#
+        self.fig = fig
+        self.ax = fig.add_subplot(111)
+
+        xlim(-10, 10)
+        ylim(-10, 10)
+        connect('key_press_event', lambda event : self.gate_drawer(event, self.ax))
+        show()
+
+    def create_vertex(self, event):
+        ax = self.ax
+        #rect = DraggableRectangle.from_vert(plt.gca(), (event.xdata, event.ydata, 0.3, 0.3))
+        rect = Vertex((event.xdata, event.ydata), ax)
+        self.gates.append(rect)
+        self.cs.clear(event)
+        self.cs.disconnect_events()
+        del self.cs
+        self.fig.canvas.draw()
+
+    def create_polygon(self, poly_drawer_instance):
+        ax = self.ax
+        verts = poly_drawer_instance.verts
+        pg = PolyGate(verts, ax)
+        self.pg = pg
+        del poly_drawer_instance
+
+    def create_threshold_gate(self, event, orientation, ax):
+        gate = ThresholdGate((event.xdata, event.ydata), orientation, ax=ax)
+        self.gates.append(gate)
+        self.cs.clear(event)
+        self.cs.disconnect_events()
+        del self.cs
+        gcf().canvas.draw()
+
+    def load_fcs(self, parent=None):
+        ax = self.ax
+
+        if parent is None:
+            parent = self.fig.canvas
+
+        from GoreUtilities import dialogs
+        filepath = dialogs.open_file_dialog('Select an FCS file to load',
+                    'FCS files (*.fcs)|*.fcs', parent=parent)
+
+        if filepath is not None:
+            self.sample = FCMeasurement('temp', datafile=filepath)
+            self._sample_loaded_event()
+
+    def gate_drawer(self, event, ax):
+        """
+        Launches different drawing tools
+        """
+        if event.key.lower() in ['1']:
+            self.pd = PolyDrawer(ax, oncreated=self.create_polygon, lineprops = dict(color='k', marker='o'))
+        elif event.key in ['2', '3', '4']:
+            orientation = {'2' : 'both', '3' : 'horizontal', '4' : 'vertical'}[event.key]
+
+            if not hasattr(self, 'cs'):
+                vertOn  = event.key in ['2', '4']
+                horizOn = event.key in ['2', '3']
+                self.cs = Cursor(ax, vertOn=vertOn, horizOn=horizOn)
+                self.cs.connect_event('button_press_event',
+                        lambda event : self.create_threshold_gate(event, orientation, ax))
+        elif event.key in ['C', 'c']:
+            if not hasattr(self, 'cs'):
+                self.cs = Cursor(ax)
+                self.cs.connect_event('button_press_event', self.create_vertex)
+        elif event.key in ['0']:
+            self.load_fcs(ax, pl.gcf().canvas)
+        #if event.key in ['R', 'r']:
+            #print 'Launching rectangle selector'
+            #onselect.RS = RectangleSelector(ax, onselect, drawtype='box')
+
+
+    @staticmethod
+    def plot_data():
+        """ Plots the loaded data """
+        sample = self.sample
+
+        ax = self.ax
+
+        if self._plt_data is not None:
+            if isinstance(self._plt_data, tuple):
+                # This is the case for histograms which return a tuple
+                patches = self._plt_data[2]
+                map(lambda x : x.remove(), patches)
+            else:
+                self._plt_data.remove()
+            del self._plt_data
+            self._plt_data = None
+
+        channels = self.current_channels
+
+        if channels[0] == channels[1]:
+            self._plt_data = sample.plot(channels[0], ax=ax)
+            xlabel = self.current_channels[0]
+            ylabel = 'Counts'
+        else:
+            self._plt_data = sample.plot(channels, ax=ax)
+            xlabel = self.current_channels[0]
+            ylabel = self.current_channels[1]
+
+
+
+        if hasattr(self._plt_data, 'get_datalim'):
+            bbox = self._plt_data.get_datalim(self.ax.transData)
+            p0 = bbox.get_points()[0]
+            p1 = bbox.get_points()[1]
+
+            self.ax.set_xlim(p0[0], p1[0])
+            self.ax.set_ylim(p0[1], p1[1])
+        else:
+            # Then it's a histogram?
+            xlims = self._plt_data[1]
+            xlims = (xlims[0], xlims[-1])
+            self.ax.set_xlim(xlims)
+            self.ax.set_ylim(0, max(self._plt_data[0]))
+
+        self.fig.canvas.draw()
+
 if __name__ == '__main__':
-    fig = figure()
-    ax = fig.add_subplot(111)
-    xlim(-10, 10)
-    ylim(-10, 10)
-    #a = Vertex((0.3, 0.4), ax)
-    connect('key_press_event', gate_drawer)
-    #mn = LassoSelector(ax)
-    show()
+    manager = Manager()
