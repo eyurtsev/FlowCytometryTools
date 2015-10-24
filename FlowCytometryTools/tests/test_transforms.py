1bdb95a12cd5c2a1dec2dfdb359179de36d9e87a
Eugene Yurtsev <eyurtsev@gmail.com>
2015-10-24 18:27:38 -0400

    refactoring transform tests

--- FlowCytometryTools/tests/test_transforms.py
+++ FlowCytometryTools/tests/test_transforms.py
@@ -6,8 +6,11 @@ import unittest
 import numpy as np
 from numpy.testing import assert_almost_equal, assert_equal
 
+from FlowCytometryTools import FCMeasurement
 from FlowCytometryTools.core import transforms as trans
 from FlowCytometryTools.core.transforms import Transformation
+
+test_path = '../tests/data/FlowCytometers/HTS_BD_LSR-II/HTS_BD_LSR_II_Mixed_Specimen_001_D6_D06.fcs'
 
 n = 1000
 _xmax = 2 ** 18  # max machine value
@@ -88,3 +91,17 @@ class TestTransforms(unittest.TestCase):
         result = trans.hlog_inv(trans.hlog(_xall, b=10), b=10)
         d = (result - expected) / expected
         assert_almost_equal(d, np.zeros(len(d)), decimal=2)
+
+    def test_hlog_on_fc_measurement(self):
+        fc_measurement = FCMeasurement(ID='test', datafile=test_path)
+        fc_measurement = fc_measurement.transform(transform='hlog', b=10)
+        data = fc_measurement.data.values[:3, :4]
+        correct_output = np.array([[-8.22113965e+03, 1.20259949e+03, 1.01216449e-06,
+                                    5.21899170e+03],
+                                   [-8.66184277e+03, 1.01013794e+03, 1.01216449e-06,
+                                    5.71275928e+03],
+                                   [-8.79974414e+03, 1.52737976e+03, 1.01216449e-06,
+                                    -4.95852930e+03]])
+        np.testing.assert_array_almost_equal(data, correct_output, 5,
+                                             err_msg='the hlog transformation gives '
+                                                     'an incorrect result')
