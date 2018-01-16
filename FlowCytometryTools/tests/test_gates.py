719906ea0249d4e3443addcc4e8958eedd0c9bc9
epgui <guigui.p@gmail.com>
2018-01-16 07:19:36 -0400

    Adjust tests for python 3 compatibility

--- FlowCytometryTools/tests/test_gates.py
+++ FlowCytometryTools/tests/test_gates.py
@@ -15,13 +15,13 @@ class TestGates(unittest.TestCase):
         test_df = pd.DataFrame({'channel': [-1, 0.5, 1.5]}, index=[1, 2, 3])
 
         test_cases = (
-            (0.0, 1.0, 'channel', 'in', 'gate1', [2]),
-            (-10.0, 1.0, 'channel', 'in', 'gate1', [1, 2]),
-            (-10.0, 10.0, 'channel', 'in', 'gate1', [1, 2, 3]),
-            (10.0, 30.0, 'channel', 'in', 'gate1', []),
-            (10.0, 30.0, 'channel', 'out', 'gate1', [1, 2, 3]),
-            (0, 1, 'channel', 'out', 'gate1', [1, 3]),
-            (-10, 10, 'channel', 'out', 'gate1', []),
+            (0.0, 1.0, ['channel'], 'in', 'gate1', [2]),
+            (-10.0, 1.0, ['channel'], 'in', 'gate1', [1, 2]),
+            (-10.0, 10.0, ['channel'], 'in', 'gate1', [1, 2, 3]),
+            (10.0, 30.0, ['channel'], 'in', 'gate1', []),
+            (10.0, 30.0, ['channel'], 'out', 'gate1', [1, 2, 3]),
+            (0, 1, ['channel'], 'out', 'gate1', [1, 3]),
+            (-10, 10, ['channel'], 'out', 'gate1', []),
         )
 
         for vert0, vert1, channel_name, region, gate_name, expected_output in test_cases:
@@ -42,5 +42,5 @@ class TestGates(unittest.TestCase):
 
         # Check that gate behaves correctly with empty dataframe
         empty_df = pd.DataFrame({'channel': []}, index=[])
-        gate = IntervalGate((0, 1), 'channel', 'in')
+        gate = IntervalGate((0, 1), ['channel'], 'in')
         self.assertEqual(_get_indexes_where_true(gate._identify(empty_df)), [])
