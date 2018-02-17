6dffb6929fc3e4665f09ed9a4817752ccf36dde4
Eugene Yurtsev <eyurtsev@gmail.com>
2018-02-17 13:46:23 -0500

    convert to actual test

--- FlowCytometryTools/tests/test_imports.py
+++ FlowCytometryTools/tests/test_imports.py
@@ -1,3 +1,9 @@
 """Shallow tests that at least attempt to import some code."""
-from FlowCytometryTools.gui import dialogs, fc_widget  # noqa
-from FlowCytometryTools.core import graph  # noqa
+import unittest
+
+
+class TestImports(unittest.TestCase):
+    def test_imports(self):
+        from FlowCytometryTools.gui import dialogs, fc_widget  # noqa
+        from FlowCytometryTools.core import (graph, gates, bases, containers, docstring,
+                                             transforms)  # noqa
