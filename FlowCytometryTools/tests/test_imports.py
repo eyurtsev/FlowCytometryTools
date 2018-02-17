e9383da25acdf848681ce27172f2da1ef7d3189f
Eugene Yurtsev <eyurtsev@gmail.com>
2018-02-17 13:17:16 -0500

    Add noqa flags

--- FlowCytometryTools/tests/test_imports.py
+++ FlowCytometryTools/tests/test_imports.py
@@ -1,4 +1,3 @@
 """Shallow tests that at least attempt to import some code."""
-from FlowCytometryTools.gui import dialogs, fc_widget
-from FlowCytometryTools.utility_lib import graph
-
+from FlowCytometryTools.gui import dialogs, fc_widget  # noqa
+from FlowCytometryTools.utility_lib import graph  # noqa
