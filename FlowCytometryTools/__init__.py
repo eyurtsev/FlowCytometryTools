9ab9f9b2149150f466c63c738d3da43d0ec9d391
Eugene Yurtsev <eyurtsev@gmail.com>
2018-02-17 13:57:40 -0500

    remove os from __init__.py

--- FlowCytometryTools/__init__.py
+++ FlowCytometryTools/__init__.py
@@ -3,15 +3,22 @@ from __future__ import absolute_import
 from FlowCytometryTools._version import version as __version__
 from FlowCytometryTools._doc import __doc__
 
-import os
 from fcsparser.api import parse as parse_fcs
 
-from FlowCytometryTools.core.containers import FCMeasurement, FCCollection, FCOrderedCollection, FCPlate
+from FlowCytometryTools.core.containers import (FCMeasurement, FCCollection, FCOrderedCollection,
+                                                FCPlate)
 from FlowCytometryTools.core.gates import ThresholdGate, IntervalGate, QuadGate, PolyGate
 import FlowCytometryTools.core.graph as graph
 from FlowCytometryTools.core.graph import plotFCM
 
-_base_path = os.path.dirname(os.path.abspath(__file__))
 
-test_data_dir = os.path.join(_base_path, 'tests', 'data', 'Plate01')
-test_data_file = os.path.join(test_data_dir, 'RFP_Well_A3.fcs')
+def _get_paths():
+    """Generate paths to test data. Done in a function to protect namespace a bit."""
+    import os
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    test_data_dir = os.path.join(base_path, 'tests', 'data', 'Plate01')
+    test_data_file = os.path.join(test_data_dir, 'RFP_Well_A3.fcs')
+    return test_data_dir, test_data_file
+
+
+test_data_dir, test_data_file = _get_paths()
