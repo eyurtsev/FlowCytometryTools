815d5bea357dddbe0e2d1146317c0843df9e0a6a
Eugene Yurtsev <eyurtsev@gmail.com>
2018-02-17 19:32:49 -0500

    versio 0.5.0

--- FlowCytometryTools/_doc.py
+++
@@ -1,3 +0,0 @@
-"""
-A package for analyzing flow cytometry tools.
-"""
--- FlowCytometryTools/tests/test_imports.py
+++
@@ -1,9 +0,0 @@
-"""Shallow tests that at least attempt to import some code."""
-import unittest
-
-
-class TestImports(unittest.TestCase):
-    def test_imports(self):
-        from FlowCytometryTools.gui import dialogs, fc_widget  # noqa
-        from FlowCytometryTools.core import (graph, gates, bases, containers, docstring,
-                                             transforms)  # noqa
